818a43ebce2aa3223275d7cf1eb7f73b49820a44
Mauricio Barahona Irizarri <mfbarahonai@gmail.com>
2021-03-25 23:17:59 -0600

    creates first failing unittest

---
+++ test_library.py
@@ -0,0 +1,18 @@
+
+
+# Standard python library
+import unittest
+
+# local application imports
+from checkurl import verify_signature, ERROR_URL
+
+
+class signature_verification(unittest.TestCase):
+	"""docstring for signature_verification"""
+	def __init__(self, arg):
+		super(signature_verification, self).__init__()
+		self.arg = arg
+
+	def test_output_is_an_error_url_from_unverified_signature(self):
+		bad_url_signature = 'http://example.com/?b02k_mac=invalidsignature'
+		self.assertEqual(ERROR_URL, verify_signature(bad_url_signature))
