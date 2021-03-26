6383b66a93024060d2835a8fb1c52b6424d44353
Mauricio Barahona Irizarri <mfbarahonai@gmail.com>
2021-03-26 16:45:20 -0600

    failing test for extract function

--- checkurl.py
+++ checkurl.py
@@ -1,7 +1,19 @@
 
 
+import hashlib
+
 
 ERROR_URL = 'http://error'
 
-def verify_signature(given_url):
-    return ERROR_URL
+def extract(given_url):
+    pass
+
+def valid_signature(given_url):
+    # B02K_VERS, B02K_TIMESTMP, B02K_IDNBR, 
+    # B02K_STAMP, B02K_CUSTNAME, B02K_KEYVERS, 
+    # B02K_ALG, B02K_CUSTID, B02K_CUSTTYPE, B02K_MAC
+    extracted_data = extract(given_url)
+    B02K_MAC = extracted_data.popitem()
+    encoded = "".join(extracted_data.values()).encode('cp1252')
+    calculated_signature = hashlib.sha256(encoded)
+    return False
--- test_library.py
+++ test_library.py
@@ -4,12 +4,22 @@
 import unittest
 
 # local application imports
-from checkurl import verify_signature, ERROR_URL
+from checkurl import valid_signature, extract
 
 
-class signature_verification(unittest.TestCase):
+class Url_reading(unittest.TestCase):
+    """docstring for Url_reading"""
+    def test_extract_returns_all_10_items_from_valid_url(self):
+        valid_url = 'http://someserver.com/?B02K_VERS=0003&B02K_TIMESTMP=50020181017141433899056&B02K_IDNBR=2512408990&B02K_STAMP=20010125140015123456&B02K_CUSTNAME=FIRST%20LAST&B02K_KEYVERS=0001&B02K_ALG=03&B02K_CUSTID=9984&B02K_CUSTTYPE=02&B02K_MAC=EBA959A76B87AE8996849E7C0C08D4AC44B053183BE12C0DAC2AD0C86F9F2542'
+        self.assertEqual(len(extract(valid_url)), 10)
+
+class Signature_verification(unittest.TestCase):
     """docstring for signature_verification"""
 
     def test_output_is_error_url_from_unverified_signature(self):
-        bad_url_signature = 'http://example.com/?b02k_mac=invalidsignature'
-        self.assertEqual(ERROR_URL, verify_signature(bad_url_signature))
+        corrupted_data = 'http://example.com/?b02k_custname=mrmalware&b02k_mac=267d3b81a9dcd937f3b46a17a57fc0ca2133373389336861142673a73fc17bc6'
+        self.assertFalse(valid_signature(corrupted_data))
+
+    def test_a_valid_signature(self):
+        normal_data = ''
+        self.assertTrue(valid_signature(normal_data))
