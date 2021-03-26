1542cf0e8842eead9e2e9ff2fc2c693a6ecf01d8
Mauricio Barahona Irizarri <mfbarahonai@gmail.com>
2021-03-26 11:15:50 -0600

    just changes the test name

--- test_library.py
+++ test_library.py
@@ -10,6 +10,6 @@ from checkurl import verify_signature, ERROR_URL
 class signature_verification(unittest.TestCase):
     """docstring for signature_verification"""
 
-    def test_output_is_an_error_url_from_unverified_signature(self):
+    def test_output_is_error_url_from_unverified_signature(self):
         bad_url_signature = 'http://example.com/?b02k_mac=invalidsignature'
         self.assertEqual(ERROR_URL, verify_signature(bad_url_signature))
