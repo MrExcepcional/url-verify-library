43fbd781b9d337d57e6228a4d812157f93da2ee4
Mauricio Barahona Irizarri <mfbarahonai@gmail.com>
2021-03-25 23:28:25 -0600

    erases the auto-generated __init__() to avoid any conflict with TestCase setup

--- test_library.py
+++ test_library.py
@@ -8,11 +8,8 @@ from checkurl import verify_signature, ERROR_URL
 
 
 class signature_verification(unittest.TestCase):
-	"""docstring for signature_verification"""
-	def __init__(self, arg):
-		super(signature_verification, self).__init__()
-		self.arg = arg
+    """docstring for signature_verification"""
 
-	def test_output_is_an_error_url_from_unverified_signature(self):
-		bad_url_signature = 'http://example.com/?b02k_mac=invalidsignature'
-		self.assertEqual(ERROR_URL, verify_signature(bad_url_signature))
+    def test_output_is_an_error_url_from_unverified_signature(self):
+        bad_url_signature = 'http://example.com/?b02k_mac=invalidsignature'
+        self.assertEqual(ERROR_URL, verify_signature(bad_url_signature))
