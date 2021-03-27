3efce577e03eff42c4ffd2bbc5ba8079e2dc3f43
Mauricio Barahona Irizarri <mfbarahonai@gmail.com>
2021-03-26 23:23:32 -0600

    Adds try-except block to handle incomplete or wrong urls. _extract returns False when fails to obtain data

--- checkurl.py
+++ checkurl.py
@@ -6,25 +6,26 @@ import hashlib
 ERROR_URL = 'http://error'
 
 def _extract(given_url):
-    # We just trim the leading domain
-    raw_data = given_url.split('?')[1]
-    # Then separate each data element
-    raw_data = raw_data.split('&')
-    # separate keys from values.
-    # TODO: find better sintax for this block
-    keys,values=[],[]
-    for item in raw_data:
-        key_value_tuple = item.split('=')
-        keys.append(key_value_tuple[0])
-        values.append(key_value_tuple[1])
-    # Now store values and keys in a dict
-    data = dict(zip(keys, values))
-    return data
+    try:
+        # We just trim the leading domain
+        raw_data = given_url.split('?')[1]
+        # Then separate each data element
+        raw_data = raw_data.split('&')
+        # separate keys from values.
+        # TODO: find better sintax for this block
+        keys,values=[],[]
+        for item in raw_data:
+            key_value_tuple = item.split('=')
+            keys.append(key_value_tuple[0])
+            values.append(key_value_tuple[1])
+        # Now store values and keys in a dict
+        data = dict(zip(keys, values))
+        return data
+    except Exception as e:
+        print(e)
+        return False
 
 def is_valid_signature(given_url):
-    # B02K_VERS, B02K_TIMESTMP, B02K_IDNBR, 
-    # B02K_STAMP, B02K_CUSTNAME, B02K_KEYVERS, 
-    # B02K_ALG, B02K_CUSTID, B02K_CUSTTYPE, B02K_MAC
     extracted_data = _extract(given_url)
     #if extracted_data:
     B02K_MAC = extracted_data.popitem()
